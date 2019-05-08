ed9f92518e1c6a2348f23f1484ee9bf3dad838d1
Maykon de Oliveira Mendonça <maykon.oliveira@academico.ifrn.edu.br>
2019-05-08 14:23:58 -0300

    Add arquivo de configuração do vscode para o mocha

--- test/integration/helpers.js
+++ test/integration/helpers.js
@@ -3,5 +3,5 @@ const chai = require('chai');
 const app = require('../../src/app.js');
 
 global.app = app;
-global.request = supertest(app);
+global.servidor = supertest(app);
 global.expect = chai.expect;
