056738b3c4a2d0a16eb5c06262cc89227a768b2b
João Renato <=>
2019-06-17 16:06:50 -0300

    branch todo-delete

--- src/rotas/todos.js
+++ src/rotas/todos.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const TodoController = require('../controllers/todo-controller');
 
 router.get('/', TodoController.findAll);
+router.post('/',TodoController.post);
+router.delete('/:id',TodoController.delete);
 
 module.exports = router;
