9caddaf2167a3d49ae114e35c034a086c1cfc166
Maykon de Oliveira Mendonça <maykon.oliveira@academico.ifrn.edu.br>
2019-06-17 15:51:38 -0300

    Mudanca no codigo

--- src/controllers/todo-controller.js
+++ src/controllers/todo-controller.js
@@ -6,18 +6,13 @@ const todos = [
         completo: false
     }
 ]
-const novoTodo = {
-    id: 0,
-    titulo: 'Apredendo a testar APIs rest com nodejs',
-    dataCriacao: '2019-06-16T17:18:06.569Z',
-    completo: false
-}
+
 module.exports = {
     async findAll(req, res) {
         res.status(200).send(todos);
     },
-    async createATask(req, res) {
-        todos.push([novoTodo]);
-        res.status(201).send([novoTodo]);
+    async createTodo(req, res) {
+        todos.push(req.body);
+        res.status(201).send(req.body);
     }
 }
--- src/rotas/todos.js
+++ src/rotas/todos.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const TodoController = require('../controllers/todo-controller');
 
 router.get('/', TodoController.findAll);
-router.post('/', TodoController.createATask);
+router.post('/', TodoController.createTodo);
 
 module.exports = router;
