48c4316c216726bcf39825a76c2513ff7fa3d35e
Leandro Bezerra <leandrobezerra94@hotmail.com>
2019-06-17 11:23:04 -0300

    Teste unitário e de integração para o método post

--- src/controllers/todo-controller.js
+++ src/controllers/todo-controller.js
@@ -6,9 +6,18 @@ const todos = [
         completo: false
     }
 ]
-
+const novoTodo = {
+    id: 0,
+    titulo: 'Apredendo a testar APIs rest com nodejs',
+    dataCriacao: '2019-06-16T17:18:06.569Z',
+    completo: false
+}
 module.exports = {
     async findAll(req, res) {
         res.status(200).send(todos);
+    },
+    async createATask(req, res) {
+        todos.push([novoTodo]);
+        res.status(201).send([novoTodo]);
     }
 }
